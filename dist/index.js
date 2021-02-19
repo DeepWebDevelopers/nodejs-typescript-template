568c3da4569a303d81621b72ffe6652f566dcd61
Jamal L <jamallyons8@gmail.com>
2021-02-19 08:16:31 -0500

    Changed dist

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log("Hello from typescript!");
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 5)); // result 10
+//? All this code will be compiled into "../dist/index.js"
