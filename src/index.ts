5d684d1f973b9f05130f19d8d6e2ca7eab4ca62c
Jamal L <jamallyons8@gmail.com>
2021-02-19 08:03:00 -0500

    Hello world!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 console.log("Hello from typescript!");
 
+function add(a: number, b:number) {
+    return a + b
+}
+
+console.log(add(5, 5)) // result 10
 
 //? All this code will be compiled into "../dist/index.js"
