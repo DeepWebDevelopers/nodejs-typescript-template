9a158b4dbd18c9b09d75ee156696dd7712d26414
Jamal L <jamallyons8@gmail.com>
2021-02-19 23:30:07 -0500

    adding types

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
+//! Commenting out code to test express.
+
 console.log("Hello from typescript!");
 
-function add(a: number, b: number) {
-	return a + b;
-}
+// function add(a: number, b: number) {
+// 	return a + b;
+// }
 
-interface Car {
-	year: number;
-	model: string | number;
-	electric?: boolean;
-}
+// interface Car {
+// 	year: number;
+// 	model: string | number;
+// 	electric?: boolean;
+// }
 
-let myCar: Car = {
-	year: 5,
-	model: "one",
-	electric: true,
-};
+// let myCar: Car = {
+// 	year: 5,
+// 	model: "one",
+// 	electric: true,
+// };
 
-console.log(`${myCar.year}`);
+// console.log(`${myCar.year}`);
 
-console.log(add(5, 5)); // result 10
+// console.log(add(5, 5)); // result 10
 
-//? All this code will be compiled into "../dist/index.js"
+// //? All this code will be compiled into "../dist/index.js"
